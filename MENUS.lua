d2a3c9c7b1c65c50e9a1b248428f978facf56966
Syuan.Tsai <Syuan.Tsai@dash.com.au>
2025-04-12 00:37:15 +0800

    Update Menus 60%

--- MENUS.lua
+++ MENUS.lua
@@ -13,6 +13,7 @@ COLORS_KWords = mod:io_dofile("Enhanced_descriptions/Loc_EN/COLORS_KWords")
 
 	--[+ Translations +]-- Add a line with a file of Keywords translated into your language.
 COLORS_KWords_fr = mod:io_dofile("Enhanced_descriptions/Loc_FR/COLORS_KWords_fr") -- French
+COLORS_KWords_tw = mod:io_dofile("Enhanced_descriptions/Loc_TW/COLORS_KWords_tw_v2") -- Traditional Chinese
 
 --[+ Function to create a localization template +]--
 local function create_template(id, loc_keys, locales, handle_func)
@@ -27,75 +28,158 @@ local localization_templates = {
 
 -- Make changes to this file only if you need them!
 
---[+ MELK +]--
+--[+ MELK - 梅爾克 +]--
 	-- HERE IS THE NUMBER HIGHLIGHT IN THE CONTRACTS MENU!
-	--[+ Contracts +]-- Highlighted numbers
-		--[+ Kill Х Monstrosities +]--
+	--[+ Contracts - 合約 +]-- Highlighted numbers
+	
+	--[+ Kill Х Monstrosities +]--
 	create_template("contract_melk_task0_ext_en", {"loc_contracts_task_label_kill_bosses"}, {"en",}, function(locale, value) return "Kill "..COLORS_Numbers.countd_var_rgb.." Monstrosities" end),
-	--[+ Russian +]--
+		--[+ Russian +]--
 	create_template("contract_melk_task0_ext_ru", {"loc_contracts_task_label_kill_bosses"}, {"ru",}, function(locale, value) return "Убить "..COLORS_Numbers.countd_var_rgb.." монстров" end),
-		--[+ Collect Х kind +]--
-	create_template("contract_melk_task1_ext_en", {"loc_contracts_task_label_collect_resources"}, {"en",}, function(locale, value) return "Collect "..COLORS_Numbers.counts_var_rgb.." {kind:%s}" end),
-	--[+ Russian +]--
-	create_template("contract_melk_task1_ext_ru", {"loc_contracts_task_label_collect_resources"}, {"ru",}, function(locale, value) return "{kind:%s}: собрать "..COLORS_Numbers.counts_var_rgb.." единиц" end),
-		--[+ Kill Х enemy_type with weapon_type +]--
-	create_template("contract_melk_task2_ext_en", {"loc_contracts_task_label_kill_minions"}, {"en",}, function(locale, value) return "Kill "..COLORS_Numbers.countd_var_rgb.." {enemy_type:%s} with {weapon_type:%s}" end),
-	--[+ Russian +]--
-	create_template("contract_melk_task2_ext_ru", {"loc_contracts_task_label_kill_minions"}, {"ru",}, function(locale, value) return "{weapon_type:%s}: убейте "..COLORS_Numbers.countd_var_rgb.." врагов типа {enemy_type:%s}" end),
-		--[+ Complete Х missions +]--
-	create_template("contract_melk_task3_ext_en", {"loc_contracts_task_label_complete_missions"}, {"en",}, function(locale, value) return "Complete "..COLORS_Numbers.countd_var_rgb.." missions" end),
-	--[+ Russian +]--
-	create_template("contract_melk_task3_ext_ru", {"loc_contracts_task_label_complete_missions"}, {"ru",}, function(locale, value) return "Завершите "..COLORS_Numbers.countd_var_rgb.." миссий" end),
-		--[+ Complete Х missions with no player deaths +]--
-	create_template("contract_melk_task4_ext_en", {"loc_contracts_task_label_complete_mission_no_death"}, {"en",}, function(locale, value) return "Complete "..COLORS_Numbers.countd_var_rgb.." missions with no player deaths" end),
-	--[+ Russian +]--
-	create_template("contract_melk_task4_ext_ru", {"loc_contracts_task_label_complete_mission_no_death"}, {"ru",}, function(locale, value) return "Завершите миссии без гибели союзников: "..COLORS_Numbers.countd_var_rgb end),
-		--[+ Pick up Х kind +]--
-	create_template("contract_melk_task5_ext_en", {"loc_contracts_task_label_collect_pickups"}, {"en",}, function(locale, value) return "Pick up "..COLORS_Numbers.counts_var_rgb.." {kind:%s}" end),
-	--[+ Russian +]--
-	create_template("contract_melk_task5_ext_ru", {"loc_contracts_task_label_collect_pickups"}, {"ru",}, function(locale, value) return "{kind:%s}: собрать "..COLORS_Numbers.counts_var_rgb.." штук" end),
+		--[+ 擊殺巨獸 - Traditional Chinese +]--
+	create_template("contract_melk_task0_ext_tw", {"loc_contracts_task_label_kill_bosses"}, {"zh-tw",}, function(locale, value) return "擊殺 "..COLORS_Numbers.countd_var_rgb.." 巨獸(畸形怪獸)" end),
 
-	--[+ Menu +]--
-	--[+ Russian +]--
+	--[+ Collect Х kind +]--
+	create_template("contract_melk_task1_ext_en", {"loc_contracts_task_label_collect_resources"}, {"en",}, function(locale, value) return "Collect "..COLORS_Numbers.counts_var_rgb.." {kind:%s}" end),
+		--[+ Russian +]--
+	create_template("contract_melk_task1_ext_ru", {"loc_contracts_task_label_collect_resources"}, {"ru",}, function(locale, value) return "{kind:%s}: собрать "..COLORS_Numbers.counts_var_rgb.." единиц" end),
+		--[+ - 蒐集[x]數量 - Traditional Chinese +]--
+	create_template("contract_melk_task1_ext_tw", {"loc_contracts_task_label_collect_resources"}, {"zh-tw",}, function(locale, value) return "收集 "..COLORS_Numbers.counts_var_rgb.." {kind:%s}" end),
+	
+	--[+ Kill Х enemy_type with weapon_type +]--
+	create_template("contract_melk_task2_ext_en", {"loc_contracts_task_label_kill_minions"}, {"en",}, function(locale, value) return "Kill "..COLORS_Numbers.countd_var_rgb.." {enemy_type:%s} with {weapon_type:%s}" end),
+		--[+ Russian +]--
+	create_template("contract_melk_task2_ext_ru", {"loc_contracts_task_label_kill_minions"}, {"ru",}, function(locale, value) return "{weapon_type:%s}: убейте "..COLORS_Numbers.countd_var_rgb.." врагов типа {enemy_type:%s}" end),
+		--[+ 使用[遠/近]擊殺[x][血痂/渣仔] - Traditional Chinese +]--
+	create_template("contract_melk_task2_ext_tw", {"loc_contracts_task_label_kill_minions"}, {"zh-tw",}, function(locale, value) return "使用 {weapon_type:%s} 擊殺 "..COLORS_Numbers.countd_var_rgb.." {enemy_type:%s}" end),
+
+	--[+ Complete Х missions +]--
+	create_template("contract_melk_task3_ext_en", {"loc_contracts_task_label_complete_missions"}, {"en",}, function(locale, value) return "Complete "..COLORS_Numbers.countd_var_rgb.." missions" end),
+		--[+ Russian +]--
+	create_template("contract_melk_task3_ext_ru", {"loc_contracts_task_label_complete_missions"}, {"ru",}, function(locale, value) return "Завершите "..COLORS_Numbers.countd_var_rgb.." миссий" end),
+		--[+ 完成[x]場任務 - Traditional Chinese +]--
+	create_template("contract_melk_task3_ext_tw", {"loc_contracts_task_label_complete_missions"}, {"zh-tw",}, function(locale, value) return "完成 "..COLORS_Numbers.countd_var_rgb.." 任務" end),
+
+	--[+ Complete Х missions with no player deaths +]--
+	create_template("contract_melk_task4_ext_en", {"loc_contracts_task_label_complete_mission_no_death"}, {"en",}, function(locale, value) return "Complete "..COLORS_Numbers.countd_var_rgb.." missions with no player deaths" end),
+		--[+ Russian +]--
+	create_template("contract_melk_task4_ext_ru", {"loc_contracts_task_label_complete_mission_no_death"}, {"ru",}, function(locale, value) return "Завершите миссии без гибели союзников: "..COLORS_Numbers.countd_var_rgb end),
+		--[+ 完成[x]場任務，並且無人死亡 - Traditional Chinese +]--
+	create_template("contract_melk_task4_ext_tw", {"loc_contracts_task_label_complete_mission_no_death"}, {"zh-tw",}, function(locale, value) return "完成 "..COLORS_Numbers.countd_var_rgb.." 任務，且無玩家死亡" end),
+
+	--[+ Pick up Х kind +]--
+	create_template("contract_melk_task5_ext_en", {"loc_contracts_task_label_collect_pickups"}, {"en",}, function(locale, value) return "Pick up "..COLORS_Numbers.counts_var_rgb.." {kind:%s}" end),
+		--[+ Russian +]--
+	create_template("contract_melk_task5_ext_ru", {"loc_contracts_task_label_collect_pickups"}, {"ru",}, function(locale, value) return "{kind:%s}: собрать "..COLORS_Numbers.counts_var_rgb.." штук" end),
+		--[+ 撿起[x]本[聖典/法術書] - Traditional Chinese +]--
+	create_template("contract_melk_task5_ext_tw", {"loc_contracts_task_label_collect_pickups"}, {"zh-tw",}, function(locale, value) return "撿起 "..COLORS_Numbers.counts_var_rgb.." {kind:%s}" end),
+
+	--[+ Menu - 選單 +]--
+		--[+ Russian +]--
 	create_template("contract_melk_intro_decs_ext_ru", {"loc_contract_view_intro_description",}, {"ru",}, function(locale, value) return "Ну? Чего ты хочешь?" end),
+		--[+ 好吧，你想要什麼? - Traditional Chinese +]--
+	create_template("contract_melk_intro_decs_ext_tw", {"loc_contract_view_intro_description",}, {"zh-tw",}, function(locale, value) return "好吧，你想要什麼?" end),
+
+		--[+ Russian +]--
+	create_template("contract_melk_intro_ext_ru", {"loc_contract_view_intro_title",}, {"ru",}, function(locale, value) return "Мелк" end),
+		--[+ 梅爾克 - Traditional Chinese +]--
+	create_template("contract_melk_intro_ext_tw", {"loc_contract_view_intro_title",}, {"zh-tw",}, function(locale, value) return "梅爾克" end),
+
+		--[+ Russian +]--
 	create_template("contract_melk_reward_label_ext_ru", {"loc_contracts_contract_reward_label",}, {"ru",}, function(locale, value) return "Награда за выполнение:" end),
+		--[+ 獎勵 - Traditional Chinese +]--
+
+	-- I think here missing the melee weapon?
+
+		--[+ Russian +]--
 	create_template("contract_melk_gen_goods_rand_rang_weap_ext_ru", {"loc_contracts_view_general_goods_random_ranged_weapon",}, {"ru",}, function(locale, value) return "Неизвестное оружие дальнего боя" end),
+		--[+ 未知的遠程武器 - Traditional Chinese +]--
+	create_template("contract_melk_gen_goods_rand_rang_weap_ext_tw", {"loc_contracts_view_general_goods_random_ranged_weapon",}, {"zh-tw",}, function(locale, value) return "未知的遠程武器" end),
+
+		--[+ Russian +]--
 	create_template("contract_melk_gen_goods_rand_gadget_ext_ru", {"loc_contracts_view_general_goods_random_gadget_defensive",}, {"ru",}, function(locale, value) return "Неизвестная защитная реликвия" end),
+		--[+ 未知的防禦性聖物 - Traditional Chinese +]--
+	create_template("contract_melk_gen_goods_rand_gadget_ext_tw", {"loc_contracts_view_general_goods_random_gadget_defensive",}, {"zh-tw",}, function(locale, value) return "未知的防禦性聖物" end),
+
 
 	--[+ Task difficulty +]--
-	--[+ Russian +]--
+		--[+ Russian +]--
 	create_template("contracts_melk_compl0_ext_ru", {"loc_contracts_contract_complexity_easy",}, {"ru",}, function(locale, value) return "Низкая" end),
+		--[+ Traditional Chinese +]--
+	create_template("contracts_melk_compl0_ext_tw", {"loc_contracts_contract_complexity_easy",}, {"zh-tw",}, function(locale, value) return "簡單" end),
+
+		--[+ Russian +]--
 	create_template("contracts_melk_compl1_ext_ru", {"loc_contracts_contract_complexity_medium",}, {"ru",}, function(locale, value) return "Средняя" end),
+		--[+ Traditional Chinese +]--
+	create_template("contracts_melk_compl1_ext_tw", {"loc_contracts_contract_complexity_medium",}, {"zh-tw",}, function(locale, value) return "普通" end),	
+
+		--[+ Russian +]--
 	create_template("contracts_melk_compl2_ext_ru", {"loc_contracts_contract_complexity_hard",}, {"ru",}, function(locale, value) return "Высокая" end),
+		--[+ Traditional Chinese +]--
+	create_template("contracts_melk_compl2_ext_tw", {"loc_contracts_contract_complexity_hard",}, {"zh-tw",}, function(locale, value) return "困難" end),
 
 
 --[+ HADRON +]--
 	--[+ Menu +]--
-	--[+ Russian +]--
+
+		--[+ Russian +]--
 	create_template("hud_hadron_craft1_ext_ru", {"loc_crafting_error_no_consecrate"}, {"ru"}, function(locale, value) return "Максимальный уровень редкости!" end),
+		--[+ Traditional Chinese +]--
+	create_template("hud_hadron_craft1_ext_tw", {"loc_crafting_error_no_consecrate"}, {"zh-tw"}, function(locale, value) return "已升級至最高稀有度!" end),
+	
+		--[+ Russian +]--
 	create_template("hud_hadron_craft2_ext_ru", {"loc_crafting_error_max_power"}, {"ru"}, function(locale, value) return "Максимальный уровень силы!" end),
+		--[+ Traditional Chinese +]--
+	create_template("hud_hadron_craft2_ext_tw", {"loc_crafting_error_max_power"}, {"zh-tw"}, function(locale, value) return "已升級至最高等級!" end),
+	
+		--[+ Russian +]--
 	create_template("hud_hadron_craft3_ext_ru", {"loc_mastery_crafting_sacrifice_weapon_title",}, {"ru",}, function(locale, value) return "Пожертвовать оружие" end), -- Жертвенное оружие
+		--[+ Traditional Chinese +]--
+	create_template("hud_hadron_craft3_ext_tw", {"loc_mastery_crafting_sacrifice_weapon_title",}, {"zh-tw",}, function(locale, value) return "犧牲武器" end), -- Жертвенное оружие
 
 
 --[+ ARSENAL +]--
-	--[+ Russian +]--
 	--[+ Menu +]--
+		--[+ Russian +]--
 	create_template("store_armoury_exchange_intro_decs_ext_ru", {"loc_credits_vendor_view_intro_description",}, {"ru",}, function(locale, value) return "Какая из моих скромных услуг вам требуется?" end),
+		--[+ Traditional Chinese +]--
+	create_template("store_armoury_exchange_intro_decs_ext_tw", {"loc_credits_vendor_view_intro_description",}, {"zh-tw",}, function(locale, value) return "你想要我為你提供甚麼服務" end),
+
+		--[+ Russian +]--
 	create_template("store_armoury_exchange_buy_ext_ru", {"loc_credits_vendor_view_option_buy",}, {"ru",}, function(locale, value) return "Запросы на оружие и реликвии" end),
+		--[+ Traditional Chinese +]--
+	create_template("store_armoury_exchange_buy_ext_tw", {"loc_credits_vendor_view_option_buy",}, {"zh-tw",}, function(locale, value) return "購買武器和珍品" end),
+	
+	--[+ Russian +]--
 	-- create_template("store_armoury_exchange_brunt_ext_ru", {"loc_credits_goods_vendor_title_text",}, {"ru",}, function(locale, value) return "Арсенал Бранта" end),
+	
+		--[+ Russian +]--
 	create_template("store_armoury_exchange_brunt_decs_ext_ru", {"loc_credits_goods_vendor_description_text",}, {"ru",}, function(locale, value) return "Купите нечестивое оружие по вашему выбору." end),
+		--[+ Traditional Chinese +]--
+	create_template("store_armoury_exchange_brunt_decs_ext_tw", {"loc_credits_goods_vendor_description_text",}, {"zh-tw",}, function(locale, value) return "獲得一件你選擇的武器，只會是褻瀆級(白武)。" end),
 
 
 --[+ ++MAIN MENU++ +]--
 	--[+ Russian +]--
 	--[+ Кошель +]--
 	create_template("main_menu_acc_wallet_ext_ru", {"loc_main_menu_account_wallet_title"}, {"ru"}, function(locale, value) return "Кошелёк аккаунта" end),
+	--[+ 錢包 - Traditional Chinese +]--
+	create_template("main_menu_acc_wallet_ext_tw", {"loc_main_menu_account_wallet_title"}, {"zh-tw"}, function(locale, value) return "帳號錢包" end),
+
 	--[+ Удар. группа +]--
 	create_template("main_menu_acc_warband_ext_ru", {"loc_main_menu_warband_count", "loc_social_menu_roster_view_display_name"}, {"ru"}, function(locale, value) return "Ударная группа" end),
+	--[+ 群組 - Traditional Chinese +]--
+	create_template("main_menu_acc_warband_ext_tw", {"loc_main_menu_warband_count", "loc_social_menu_roster_view_display_name"}, {"zh-tw"}, function(locale, value) return "群組" end),
+
 	--[+ Предыдущие задания +]--
 	create_template("soc_menu_warband_prev_missions_ext_ru", {"loc_social_menu_roster_players_from_previous_missions"}, {"ru"}, function(locale, value) return "Прошлые игры" end),
+	--[+ 歷史任務 - Traditional Chinese +]--
+	create_template("soc_menu_warband_prev_missions_ext_tw", {"loc_social_menu_roster_players_from_previous_missions"}, {"zh-tw"}, function(locale, value) return "歷史任務" end),
+
 	--[+ Миссии - Золотой уровень +]--
 	create_template("hud_mission_board_header_auric_ext_ru", {"loc_mission_board_view_header_tertium_hive_auric"}, {"ru"}, function(locale, value) return "Улей Терциум - Операции золотого уровня" end),
+	--[+ 奧里克任務 - Traditional Chinese +]--
+	create_template("hud_mission_board_header_auric_ext_tw", {"loc_mission_board_view_header_tertium_hive_auric"}, {"zh-tw"}, function(locale, value) return "奧里克任務" end),
 
 --[+ HAVOC - Хаос +]-- руоф Верная смерть
 	create_template("havoc_name_ext_ru", {"loc_havoc_name"}, {"ru"}, function(locale, value) return "Хаос" end),
